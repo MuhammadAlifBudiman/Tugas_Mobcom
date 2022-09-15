56359de5e04347d9aeb4dcf86b53ee16858a2dd1
MuhammadAlifBudiman <alifm2101@gmail.com>
2022-09-15 09:22:30 +0700

    initial commit

---
+++ app/src/main/java/com/example/bottomnavigation/NewActivity.java
@@ -0,0 +1,14 @@
+package com.example.bottomnavigation;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewActivity extends AppCompatActivity{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_activity);
+    }
+}
